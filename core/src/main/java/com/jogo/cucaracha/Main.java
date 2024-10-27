f0ddd0873f1f11da002eb07356426c08f6b96d02
Sr.Heitor <heitor_camanho@hotmail.com>
2024-10-27 13:23:49 -0300

    Movimentação do Inimigo

--- core/src/main/java/com/jogo/cucaracha/Main.java
+++ core/src/main/java/com/jogo/cucaracha/Main.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.jogo.cucaracha.Personagem.Inimigo;
 import com.jogo.cucaracha.Personagem.Jogador;
@@ -26,6 +29,10 @@ public class Main extends ApplicationAdapter {
     public Inimigo inimigo_personagem;
     public int contador = 1;
 
+    public float inimigo_tempo_geracao;
+
+    public Array<Sprite> inimigo_lista;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
@@ -33,7 +40,7 @@ public class Main extends ApplicationAdapter {
         tela_fase_fundo_textura = new Texture("Telas/Fase/img_fundo.png");
 
         inimigo_textura = new Texture("Inimigo/img_textura.png");
-        inimigo_personagem = new Inimigo(inimigo_textura);
+        inimigo_lista = new Array<>();
 
         jogador_textura = new Texture("Jogador/img_textura.png");
         jogador_personagem = new Jogador(jogador_textura);
@@ -42,7 +49,21 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        logica();
+        desenho();
+    }
+
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        tela_fase_fundo_textura.dispose();
+        jogador_textura.dispose();
+        inimigo_textura.dispose();
+    }
+
+    public void logica(){
+        float delta = Gdx.graphics.getDeltaTime();
 
         if(Gdx.input.isKeyJustPressed(Keys.UP)) {
             contador++;
@@ -61,19 +82,52 @@ public class Main extends ApplicationAdapter {
 
         }
 
+       for (int i = inimigo_lista.size - 1; i >= 0; i--) {
+           Sprite inimigo_movimento = inimigo_lista.get(i);
+           float inimigo_movimento_largura = inimigo_movimento.getWidth();
+           float inimigo_altura = inimigo_movimento.getHeight();
+
+           inimigo_movimento.translateX(-100f * delta);
+
+           if (inimigo_movimento.getX() < -inimigo_movimento_largura) {
+               inimigo_lista.removeIndex(i);
+           }
+       }
+
+        inimigo_tempo_geracao += delta;
+        if (inimigo_tempo_geracao > 1f) {
+            inimigo_tempo_geracao = 0;
+            inimigoGeracao();
+        }
+    }
+
+    public void desenho(){
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
         batch.begin();
         batch.draw(tela_fase_fundo_textura, 0, 0);
         batch.draw(jogador_personagem.getTextura(), jogador_temp_movimento.x, jogador_temp_movimento.y);
-        batch.draw(inimigo_personagem.getTextura(), 1400, 5);
+        for (Sprite inimigo_desenho : inimigo_lista) {
+            inimigo_desenho.draw(batch);
+        }
+
         batch.end();
     }
 
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        tela_fase_fundo_textura.dispose();
-        jogador_textura.dispose();
-        inimigo_textura.dispose();
+    public void inimigoGeracao(){
+        Sprite inimigo = new Sprite(inimigo_textura);
+        int escolha = MathUtils.random(1, 3);
+        if (escolha == 1) {
+            inimigo.setY(15);
+            inimigo.setX(1500);
+        } else if (escolha == 2) {
+            inimigo.setY(100);
+            inimigo.setX(1500);
+        }
+        else if (escolha == 3) {
+            inimigo.setY(200);
+            inimigo.setX(1500);
+        }
+        inimigo_lista.add(inimigo);
     }
 }
--- core/src/main/java/com/jogo/cucaracha/Personagem/Inimigo.java
+++ core/src/main/java/com/jogo/cucaracha/Personagem/Inimigo.java
@@ -1,6 +1,8 @@
 package com.jogo.cucaracha.Personagem;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class Inimigo {
@@ -9,12 +11,19 @@ public class Inimigo {
     private Vector2 pos3 = new Vector2(11, 195);
     private Texture textura;
 
-    public Inimigo(Texture textura){
+    /*public Inimigo(Texture textura){
         this.textura = textura;
-    }
+    }*/
 
     public Texture getTextura(){
         return this.textura;
     }
+
+    /*public void inimigoGeracao(Texture inimigo_textura){
+        Sprite inimigo = new Sprite(inimigo_textura);
+        inimigo.setY(MathUtils.random(0, 195));
+        inimigo.setX(1400);
+        inimigo_lista.add(inimigo);
+    }*/
 
 }
