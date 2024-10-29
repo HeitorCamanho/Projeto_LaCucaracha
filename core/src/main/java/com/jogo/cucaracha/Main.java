95749d3355bbbacb2e5b1b045740bb211c9436f8
Sr.Heitor <heitor_camanho@hotmail.com>
2024-10-28 21:44:28 -0300

    Reorganização do código

--- core/src/main/java/com/jogo/cucaracha/Main.java
+++ core/src/main/java/com/jogo/cucaracha/Main.java
@@ -3,12 +3,9 @@ package com.jogo.cucaracha;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
@@ -39,8 +36,8 @@ public class Main extends ApplicationAdapter {
     public Array<Sprite> inimigo_lista;
     public Array<Sprite> disparo_lista;
 
-    Rectangle inimigo_retangulo;
-    Rectangle disparo_retangulo;
+    public Rectangle inimigo_retangulo;
+    public Rectangle disparo_retangulo;
 
     boolean disparo_verifcacao;
 
@@ -52,6 +49,7 @@ public class Main extends ApplicationAdapter {
 
         inimigo_textura = new Texture("Inimigo/img_textura.png");
         inimigo_lista = new Array<>();
+        inimigo_personagem = new Inimigo(inimigo_textura);
 
         jogador_textura = new Texture("Jogador/img_textura.png");
         jogador_personagem = new Jogador(jogador_textura);
@@ -69,18 +67,22 @@ public class Main extends ApplicationAdapter {
         desenho();
     }
 
-
     @Override
     public void dispose() {
         batch.dispose();
         tela_fase_fundo_textura.dispose();
         jogador_textura.dispose();
         inimigo_textura.dispose();
+        disparo_textura.dispose();
     }
 
+//----------------------------------------------------------------------------------------
+
+    //Função responsável pela lógica principal
     public void logica(){
         float delta = Gdx.graphics.getDeltaTime();
 
+        //Trecho de captura do input do usuário
         if(Gdx.input.isKeyJustPressed(Keys.UP)) {
             contador++;
             if(contador >= 3)
@@ -96,116 +98,95 @@ public class Main extends ApplicationAdapter {
             }
             jogador_temp_movimento = jogador_personagem.jogadorPersonagemMovimento(contador);
         }
+        //Trecho de captura do input do usuário
 
-
+        //Geração e movimentação do disparo do jogador
         disparo_tempo_geracao += delta;
         if (disparo_tempo_geracao > 0.5f){
-            disparo_tempo_geracao = 0;
-            if(!disparo_verifcacao) {
-                if (contador == 1) {
-                    disparo_movimento.x = 30;
-                    disparo_movimento.y = 40;
-                    disparo_verifcacao = true;
-                } else if (contador == 2) {
-                    disparo_movimento.x = 50;
-                    disparo_movimento.y = 130;
-                    disparo_verifcacao = true;
-                } else if (contador == 3) {
-                    disparo_movimento.x = 50;
-                    disparo_movimento.y = 235;
-                    disparo_verifcacao = true;
+            disparoGeracao();
+        }
+
+        if (disparo_verifcacao){
+            disparo_movimento.x += 12;
+            if (disparo_movimento.x > 1500){
+                disparo_verifcacao = false;
+            }
+            disparo_retangulo = new Rectangle(disparo_movimento.x, disparo_movimento.y, disparo_textura.getWidth(), disparo_textura.getHeight());
+        }
+        //Geração e movimentação do disparo do jogador
+
+        //Criação e movimentação do inimigo
+        for (int i = inimigo_lista.size - 1; i >= 0; i--) {
+            Sprite inimigo_movimento = inimigo_lista.get(i);
+            float inimigo_movimento_largura = inimigo_movimento.getWidth();
+
+            inimigo_movimento.translateX(-100f * delta);
+            inimigo_retangulo = new Rectangle(inimigo_movimento.getX(), inimigo_movimento.getY(), inimigo_textura.getWidth(), inimigo_textura.getHeight());
+
+            if (inimigo_movimento.getX() < -inimigo_movimento_largura) {
+                inimigo_lista.removeIndex(i);
+            }
+
+            if (disparo_verifcacao){
+                if (disparo_retangulo.overlaps(inimigo_retangulo)) {
+                    inimigo_lista.removeIndex(i);
+                    disparo_verifcacao = false;
                 }
             }
         }
 
-        if (disparo_verifcacao){
-            disparo_movimento.x += 250 * delta;
-            disparo_retangulo = new Rectangle(disparo_movimento.x, disparo_movimento.y, disparo_textura.getWidth(), disparo_textura.getHeight());
-        }
-
-       for (int i = inimigo_lista.size - 1; i >= 0; i--) {
-           Sprite inimigo_movimento = inimigo_lista.get(i);
-           float inimigo_movimento_largura = inimigo_movimento.getWidth();
-
-           inimigo_movimento.translateX(-100f * delta);
-           inimigo_retangulo = new Rectangle(inimigo_movimento.getX(), inimigo_movimento.getY(), inimigo_textura.getWidth(), inimigo_textura.getHeight());
-
-           if (inimigo_movimento.getX() < -inimigo_movimento_largura) {
-               inimigo_lista.removeIndex(i);
-           }
-
-           if (disparo_verifcacao){
-               if (disparo_retangulo.overlaps(inimigo_retangulo)) {
-                   inimigo_lista.removeIndex(i);
-                   disparo_verifcacao = false;
-               }
-           }
-       }
-
         inimigo_tempo_geracao += delta;
         if (inimigo_tempo_geracao > 1f) {
             inimigo_tempo_geracao = 0;
-            inimigoGeracao();
+            inimigo_lista.add(inimigo_personagem.inimigoGeracao());
         }
+        //Criação e movimentação do inimigo
     }
+    //Função responsável pela lógica principal
 
+    //Função responsável por desenhar o jogo
     public void desenho(){
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
 
         batch.begin();
+        //Desenhando os detalhes básicos
         batch.draw(tela_fase_fundo_textura, 0, 0);
         batch.draw(jogador_personagem.getTextura(), jogador_temp_movimento.x, jogador_temp_movimento.y);
+        //Desenhando os detalhes básicos
+
+        //Desenhando o tiro
         if (disparo_verifcacao){
             batch.draw(disparo_textura, disparo_movimento.x, disparo_movimento.y);
         }
+        //Desenhando o tiro
 
+        //Desenhando o inimigo
         for (Sprite inimigo_desenho : inimigo_lista) {
             inimigo_desenho.draw(batch);
         }
+        //Desenhando o inimigo
         batch.end();
     }
+    //Função responsável por desenhar o jogo
 
-    public void inimigoGeracao(){
-        Sprite inimigo = new Sprite(inimigo_textura);
-        int escolha = MathUtils.random(1, 3);
-        if (escolha == 1) {
-            inimigo.setY(15);
-            inimigo.setX(1500);
-        } else if (escolha == 2) {
-            inimigo.setY(100);
-            inimigo.setX(1500);
-        } else if (escolha == 3) {
-            inimigo.setY(200);
-            inimigo.setX(1500);
-        }
-        inimigo_lista.add(inimigo);
-    }
-
-    /*public void disparoGeracao(){
-        Sprite disparo = new Sprite(disparo_textura);
-
-        if (!disparo_verifcacao){
-            if (contador == 1){
-                disparo.setY(15);
-                disparo.setX(50);
+    //Função responsável por criar o disparo do jogador
+    public void disparoGeracao(){
+        disparo_tempo_geracao = 0;
+        if(!disparo_verifcacao) {
+            if (contador == 1) {
+                disparo_movimento.x = 30;
+                disparo_movimento.y = 40;
+                disparo_verifcacao = true;
             } else if (contador == 2) {
-                disparo.setY(100);
-                disparo.setX(100);
+                disparo_movimento.x = 50;
+                disparo_movimento.y = 130;
+                disparo_verifcacao = true;
             } else if (contador == 3) {
-                disparo.setY(200);
-                disparo.setX(150);
+                disparo_movimento.x = 50;
+                disparo_movimento.y = 235;
+                disparo_verifcacao = true;
             }
         }
-        if (contador == 1){
-            disparo.setY(15);
-            disparo.setX(50);
-        } else if (contador == 2) {
-            disparo.setY(100);
-            disparo.setX(100);
-        } else if (contador == 3) {
-            disparo.setY(200);
-            disparo.setX(150);
-        }
-        disparo_lista.add(disparo);
-    }*/
+    }
+    //Função responsável por criar o disparo do jogador
 }
--- core/src/main/java/com/jogo/cucaracha/Personagem/Inimigo.java
+++ core/src/main/java/com/jogo/cucaracha/Personagem/Inimigo.java
@@ -3,27 +3,28 @@ package com.jogo.cucaracha.Personagem;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
 
 public class Inimigo {
-    private Vector2 pos1 = new Vector2(11, 0);
-    private Vector2 pos2 = new Vector2(11, 95);
-    private Vector2 pos3 = new Vector2(11, 195);
     private Texture textura;
 
-    /*public Inimigo(Texture textura){
+    public Inimigo(Texture textura){
         this.textura = textura;
-    }*/
-
-    public Texture getTextura(){
-        return this.textura;
     }
 
-    /*public void inimigoGeracao(Texture inimigo_textura){
-        Sprite inimigo = new Sprite(inimigo_textura);
-        inimigo.setY(MathUtils.random(0, 195));
-        inimigo.setX(1400);
-        inimigo_lista.add(inimigo);
-    }*/
+    public Sprite inimigoGeracao(){
+        Sprite inimigo = new Sprite(this.textura);
+        int escolha = MathUtils.random(1, 3);
+        if (escolha == 1) {
+            inimigo.setY(15);
+            inimigo.setX(1500);
+        } else if (escolha == 2) {
+            inimigo.setY(100);
+            inimigo.setX(1500);
+        } else if (escolha == 3) {
+            inimigo.setY(200);
+            inimigo.setX(1500);
+        }
+        return inimigo;
+    }
 
 }
